9a9a4cb0840c23771af32332cff6679ef602043a
Max Reichenauer <max.reichenauer98@gmail.com>
2025-04-30 22:04:01 +0200

    Solved 2020 day2 part two.

--- 2020/day2/program/partTwo.js
+++ 2020/day2/program/partTwo.js
@@ -5,24 +5,24 @@ const lineReader = require("readline").createInterface({
 const inputData = [];
 lineReader.on("line", function (line) {
 	const [passwordPolicy, password] = line.split(":");
-	const [minMaxValues, letter] = passwordPolicy.split(" ");
-	const [minValue, maxValue] = minMaxValues.split("-").map((value) => Number(value));
-	inputData.push({ passwordPolicy: { minValue, maxValue, letter }, password: password.trim() });
+	const [allowedPossitions, letter] = passwordPolicy.split(" ");
+	const [firstPosition, secondPosition] = allowedPossitions.split("-").map((value) => Number(value) - 1);
+	inputData.push({ passwordPolicy: { firstPosition, secondPosition, letter }, password: password.trim() });
 });
 
 lineReader.on("close", function () {
 	let approvedPasswords = 0;
 	for (const controll of inputData) {
-		let counter = 0;
-		for (let i = 0; i < controll.password.length; i++) {
-			if (controll.password[i] === controll.passwordPolicy.letter) {
-				counter++;
-			}
-		}
-		if (counter >= controll.passwordPolicy.minValue && counter <= controll.passwordPolicy.maxValue) {
+		const isFirstCharacter =
+			controll.password[controll.passwordPolicy.firstPosition] === controll.passwordPolicy.letter ? true : false;
+		const isLastCharacter =
+			controll.password[controll.passwordPolicy.secondPosition] === controll.passwordPolicy.letter ? true : false;
+
+		if (isFirstCharacter === true && isLastCharacter === true) {
+			continue;
+		} else if (isFirstCharacter === true || isLastCharacter === true) {
 			approvedPasswords++;
 		}
-		console.log("controll", controll);
 	}
 
 	console.log("Answer: ", approvedPasswords);
