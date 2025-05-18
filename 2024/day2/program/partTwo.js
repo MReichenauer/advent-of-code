008bc336c350ac91ea7bfdd235bc724397d932ce
Max Reichenauer <max.reichenauer98@gmail.com>
2025-05-18 21:28:13 +0200

    Created folde for 2024 day2 challenges

---
+++ 2024/day2/program/partTwo.js
@@ -0,0 +1,11 @@
+const lineReader = require("readline").createInterface({
+	input: require("fs").createReadStream("../data.txt"),
+});
+
+lineReader.on("line", function (line) {
+	console.log("Line: ", line);
+});
+
+lineReader.on("close", function () {
+	console.log("Answer: ");
+});
