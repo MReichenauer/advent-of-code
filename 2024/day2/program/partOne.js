a158553b7a31b2df795109f0aa66402f2a0bcfe7
Max Reichenauer <max.reichenauer98@gmail.com>
2025-06-01 21:50:24 +0200

    Solved 2024 day two part one.

--- 2024/day2/program/partOne.js
+++ 2024/day2/program/partOne.js
@@ -2,10 +2,40 @@ const lineReader = require("readline").createInterface({
 	input: require("fs").createReadStream("../data.txt"),
 });
 
+let amountOfSafeReports = 0;
+
 lineReader.on("line", function (line) {
-	console.log("Line: ", line);
+	const report = line.split(" ").map(Number);
+	const firstTwoValuesAbs = Math.abs(report[0] - report[1]);
+	const shouldIncrease = report[0] < report[1];
+
+	if (firstTwoValuesAbs === 0 || firstTwoValuesAbs > 3) return;
+	let isRaportSafe = true;
+
+	for (let i = 0; i < report.length - 1; i++) {
+		const currentValue = report[i];
+		const nextValue = report[i + 1];
+		const isDiffValid = Math.abs(currentValue - nextValue) >= 1 && Math.abs(currentValue - nextValue) <= 3;
+
+		if (!isDiffValid) {
+			isRaportSafe = false;
+			break;
+		}
+
+		if (shouldIncrease && nextValue < currentValue) {
+			isRaportSafe = false;
+			break;
+		}
+		if (!shouldIncrease && nextValue > currentValue) {
+			isRaportSafe = false;
+			break;
+		}
+	}
+	if (isRaportSafe) {
+		amountOfSafeReports++;
+	}
 });
 
 lineReader.on("close", function () {
-	console.log("Answer: ");
+	console.log("Answer: ", amountOfSafeReports);
 });
