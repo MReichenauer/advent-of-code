27a59904cafccecbd4253621374dfc128394bccb
Max Reichenauer <max.reichenauer98@gmail.com>
2025-08-02 15:56:28 +0200

    updated description to include part two for the 2020 day 3 challenge.

--- 2020/day3/program/partTwo.js
+++ 2020/day3/program/partTwo.js
@@ -2,28 +2,24 @@ const lineReader = require("readline").createInterface({
 	input: require("fs").createReadStream("../data.txt"),
 });
 
-const inputData = [];
+let treeCount = 0;
+let rightSlopeCount = 0;
+const calcRightSlopeIndex = (row, index) => {
+	return row[index % row.length];
+};
+const countTreesOnRow = (indexToCheck) => {
+	if (indexToCheck === "#") {
+		treeCount++;
+	}
+	rightSlopeCount += 3;
+};
 lineReader.on("line", function (line) {
-	const [passwordPolicy, password] = line.split(":");
-	const [allowedPossitions, letter] = passwordPolicy.split(" ");
-	const [firstPosition, secondPosition] = allowedPossitions.split("-").map((value) => Number(value) - 1);
-	inputData.push({ passwordPolicy: { firstPosition, secondPosition, letter }, password: password.trim() });
+	if (line.length) {
+		const indexToCheck = calcRightSlopeIndex(line, rightSlopeCount);
+		countTreesOnRow(indexToCheck);
+	}
 });
 
 lineReader.on("close", function () {
-	let approvedPasswords = 0;
-	for (const controll of inputData) {
-		const isFirstCharacter =
-			controll.password[controll.passwordPolicy.firstPosition] === controll.passwordPolicy.letter ? true : false;
-		const isLastCharacter =
-			controll.password[controll.passwordPolicy.secondPosition] === controll.passwordPolicy.letter ? true : false;
-
-		if (isFirstCharacter === true && isLastCharacter === true) {
-			continue;
-		} else if (isFirstCharacter === true || isLastCharacter === true) {
-			approvedPasswords++;
-		}
-	}
-
-	console.log("Answer: ", approvedPasswords);
+	console.log(`Result: ${treeCount}`);
 });
