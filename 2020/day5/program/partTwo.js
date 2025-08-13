38e63a8b4b13e06215095f70b06f906d91c50147
Max Reichenauer <max.reichenauer98@gmail.com>
2025-08-13 10:29:59 +0200

    Created a folder for 2020 day 5.

---
+++ 2020/day5/program/partTwo.js
@@ -0,0 +1,9 @@
+const lineReader = require("readline").createInterface({
+	input: require("fs").createReadStream("../data.txt"),
+});
+
+lineReader.on("line", function (line) {});
+
+lineReader.on("close", function () {
+	console.log("Close: Part two");
+});
