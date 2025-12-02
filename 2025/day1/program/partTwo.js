9c1ff986c285680d5a2f39db00166d9d335aa59e
Max Reichenauer <max.reichenauer98@gmail.com>
2025-12-02 09:36:08 +0100

    2025 day one part two solved.

--- 2025/day1/program/partTwo.js
+++ 2025/day1/program/partTwo.js
@@ -1,1 +1,28 @@
-// Part two
+const lineReader = require("readline").createInterface({
+	input: require("fs").createReadStream("../data.txt"),
+});
+let pointer = 50;
+let zeroCounter = 0;
+lineReader.on("line", function (line) {
+	const [dir, val] = [line[0], Number(line.slice(1))];
+
+	for (let i = 0; i < val; i++) {
+		if (dir === "L") {
+			pointer--;
+			if (pointer < 0) {
+				pointer += 100;
+			}
+			if (pointer === 0) zeroCounter++;
+		} else {
+			pointer++;
+			if (pointer >= 100) {
+				pointer -= 100;
+			}
+			if (pointer === 0) zeroCounter++;
+		}
+	}
+});
+
+lineReader.on("close", function () {
+	console.log("Answer: ", zeroCounter);
+});
