73addec28dc50b35f14da0c636855cbb6214a543
Max Reichenauer <max.reichenauer98@gmail.com>
2025-12-05 09:45:57 +0100

    2025 day three part one solved.

---
+++ 2025/day3/program/partOne.js
@@ -0,0 +1,28 @@
+const lineReader = require("readline").createInterface({
+	input: require("fs").createReadStream("../data.txt"),
+});
+
+let tot = 0;
+const findJolt = (str) => {
+	let valOne = null;
+	let valTwo = null;
+	let firstFoundIndex = null;
+	for (let i = 0; i < str.length - 1; i++) {
+		if (Number(str[i] > valOne)) {
+			firstFoundIndex = i;
+			valOne = str[i];
+		}
+	}
+	for (let i = firstFoundIndex + 1; i < str.length; i++) {
+		if (Number(str[i] > valTwo)) valTwo = str[i];
+	}
+	tot += Number(valOne + valTwo);
+};
+
+lineReader.on("line", function (line) {
+	findJolt(line);
+});
+
+lineReader.on("close", function () {
+	console.log("Close part one", tot);
+});
